3f968e31de866f4921afbadf83a4d592132a32b9
Gustavo Meneses <gdameneses@gmail.com>
2023-06-03 11:57:06 -0300

    stuck on pizza/topping creation

--- app/controllers/topping_choices_controller.rb
+++ app/controllers/topping_choices_controller.rb
@@ -4,6 +4,5 @@ class ToppingChoicesController < ApplicationController
   end
 
   def create
-    raise
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'menu', to: 'pages#menu'
   get 'pages/home'
   get 'pizzas/new'
+  get 'pizza', to: 'pizzas#create'
   get 'pizzas', to: 'pizzas#index'
   get 'drinks', to: 'drinks#index'
   get 'order/new', to: 'orders#new'
@@ -18,5 +19,6 @@ Rails.application.routes.draw do
   post 'orders', to: 'orders#create'
   post 'topping_choices', to: 'topping_choices#create'
   post 'products', to: 'products#create'
+  post 'order/create_topping_choices', to: 'orders#create_topping_choices'
   patch 'order/confirm', to: 'orders#confirm'
 end
